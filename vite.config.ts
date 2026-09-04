c34f18c107078d5aa131ce7d6c7a57f4dfd13022
devMatol <37521859+devMatol@users.noreply.github.com>
2026-09-04 16:16:53 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'pdf-lib': fileURLToPath(new URL('node_modules/pdf-lib/es/index.js', import.meta.url)),
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
     include: ['pdf-lib'],
